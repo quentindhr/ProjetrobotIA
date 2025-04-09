77e1c1baf125c4e17e28f811c2c33bd305153221
Quentin <quentin.deharo@epfedu.fr>
2025-04-10 01:34:23 +0200

    Le robot detecte son nom ainsi que quand l'utilisateur parle

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import vosk
 import sounddevice as sd
 import json
+import numpy as np
 
-SEUIL_MINIMUM_VOIX=50
+SEUIL_MINIMUM_VOIX = 50
+DEVICE_INDEX = 1 # Index du microphone à utiliser 1 pour le MAC
 
 # Initialisation du modèle Vosk (FR)
 model = vosk.Model("vosk-model-fr")
@@ -26,41 +28,32 @@ def callback(indata, frames, time_info, status):
     except Exception as e:
         print(f"Erreur dans le callback audio : {e}")
 
-def detect_trigger_word(device_index=1):
+def detect_trigger_word():
     global robot_detected
     with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
-                           channels=1, callback=callback, device=device_index):
+                           channels=1, callback=callback, device=DEVICE_INDEX):
         print("Microphone détecté. Dites 'robot' pour commencer.")
         while not robot_detected:
             sd.sleep(100)
     return True
 
-def audioRec():
+def audioRec(duration=30):
     sample_rate = 16000
-    duration = 30  # Durée d'enregistrement en secondes
-    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16',device=DEVICE_INDEX)
     sd.wait()  # Attendre la fin de l'enregistrement
-
     return audio
 
 def isSilence(audio):
     # Vérifier si le volume moyen est inférieur au seuil minimum
-    volume_moyen = audio.mean()
-    if volume_moyen < SEUIL_MINIMUM_VOIX:
-        print(volume_moyen)
-        return True
-    else:
-        print(volume_moyen)
-        return False
-
-
-
-
+    volume_moyen = np.abs(audio).mean()
+    print(f"Volume moyen : {volume_moyen}")
+    return volume_moyen < SEUIL_MINIMUM_VOIX
 
 # Exemple d'utilisation dans le main
 if __name__ == "__main__":
     if detect_trigger_word():
         audio = audioRec()
         while not isSilence(audio):
-            print("silence")
+            print("Enregistrement en cours...")
             audio = audioRec()
+        print("Silence détecté. Arrêt de l'enregistrement.")
